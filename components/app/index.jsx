8f3d0fb09005fa9b7461c72675e24fefc2d749d5
Edward Breen <ted.breen@gmail.com>
2016-02-24 11:12:04 -0800

    layout: create container for app and implement columns for content

--- components/app/index.jsx
+++ components/app/index.jsx
@@ -3,7 +3,13 @@ var React = require('react')
 module.exports = React.createClass({
   render: function render () {
     return (
-      <div className="app"></div>
+      <div className="app">
+        <div className="app-leftColumn">
+          <ul className="app-reviewList">
+          </ul>
+        </div>
+        <div className="app-rightColumn"></div>
+      </div>
     )
   }
 })
