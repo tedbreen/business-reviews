fee73fa7dc832b928327e7ddf6c8f21b33955954
Edward Breen <ted.breen@gmail.com>
2016-02-23 17:08:47 -0800

    new component: Sidebar
    assembles Avatar and UserInfo into one block

---
+++ components/sidebar/index.jsx
@@ -0,0 +1,46 @@
+var React = require('react')
+// components
+var Avatar = require('../avatar/')
+var UserInfo = require('../user-info/')
+
+module.exports = React.createClass({
+  propTypes: {
+    avatarURL: React.PropTypes.string
+  , name: React.PropTypes.string
+  , profileURL: React.PropTypes.string
+  , location: React.PropTypes.string
+  , reviewCount: React.PropTypes.number
+  , friendCount: React.PropTypes.number
+  , eliteYear: React.PropTypes.string
+  }
+
+, getDefaultProps: function getDefaultProps () {
+    return {
+      name: ''
+    , profileURL: ''
+    , location: ''
+    , reviewCount: null
+    , friendCount: null
+    , eliteYear: ''
+    }
+  }
+
+, render: function render () {
+    return (
+      <div className="sidebar">
+        <Avatar
+          url={this.props.avatarURL}
+        />
+        <UserInfo
+          name={this.props.name}
+          profileURL={this.props.profileURL}
+          location={this.props.location}
+          reviewCount={this.props.reviewCount}
+          friendCount={this.props.friendCount}
+          eliteYear={this.props.eliteYear}
+        />
+      </div>
+    )
+  }
+})
+
