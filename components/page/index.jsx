bb82f3dc409e90dc544d0048678e69b91ff04ce1
Edward Breen <ted.breen@gmail.com>
2016-02-25 16:22:27 -0800

    Page: display reviews in a list, implement "load more" button

--- components/page/index.jsx
+++ components/page/index.jsx
@@ -1,12 +1,77 @@
 var React = require('react')
+// components
+var Review = require('../review/')
+var Button = require('../button/')
+// helper function; returns array of reviews, count of remaining reviews in storage/db/cache
+var getReviews = require('../../lib/get-reviews-local-storage')
 
 module.exports = React.createClass({
-  render: function render () {
+  propTypes: {
+    initialReviews: React.PropTypes.arrayOf(React.PropTypes.shape({
+      rating: React.PropTypes.number
+    , date: React.PropTypes.number
+    , reviewText: React.PropTypes.string
+    , user: React.PropTypes.shape({
+        avatarURL: React.PropTypes.string
+      , name: React.PropTypes.string
+      , profileURL: React.PropTypes.string
+      , location: React.PropTypes.string
+      , reviewCount: React.PropTypes.number
+      , friendCount: React.PropTypes.number
+      , eliteYear: React.PropTypes.string
+      })
+    })
+  )}
+
+, getDefaultProps: function getDefaultProps () {
+    return {
+      initialReviews: []
+    }
+  }
+
+, getInitialState: function getInitialState () {
+    return {
+      reviews: this.props.initialReviews
+    , moreReviews: Boolean(window.localStorage.length)
+    }
+  }
+
+, loadMoreReviews: function loadMoreReviews () {
+    var response = getReviews()
+
+    this.setState({
+      reviews: this.state.reviews.concat(response.reviews)
+    , moreReviews: Boolean(response.reviewsRemaining)
+    })
+  }
+
+, render: function render () {
+    var reviews = this.state.reviews.map(function map (review) {
+      return (
+        <Review
+          key={review.id}
+          rating={review.rating}
+          date={review.date}
+          reviewText={review.reviewText}
+          user={review.user}
+        />
+      )
+    })
+
     return (
       <div className="page">
         <div className="page-leftColumn">
           <ul className="page-reviewList">
+            {reviews}
           </ul>
+          <div className="page-button">
+            <Button
+              handleClick={this.loadMoreReviews}
+              disabled={!this.state.moreReviews}
+            >
+              {'Load More'}
+            </Button>
+          </div>
         </div>
         <div className="page-rightColumn"></div>
       </div>
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+
 // components
 var Page = require('./components/page/')
 
-ReactDOM.render(<Page />, document.getElementById('react'))
+// constants
+var SLICE_BEGIN = 0
+var SLICE_END = 3
+
+// seed data since we are not using a server
+var data = require('./data/reviews.json')
+var seedData = require('./lib/seed-data-local-storage')
+var initialReviews = data.slice(SLICE_BEGIN, SLICE_END)
+seedData(data, SLICE_END)
+
+// render the page of reviews
+ReactDOM.render(
+  <Page initialReviews={initialReviews}/>
+, document.getElementById('react')
+)
